e3e490250aec76b3dbece9f69d586cdb778dc5b0
AineeJames <olsenaiden33@gmail.com>
2023-11-18 18:31:09 -0700

    added touch input

--- src/components/ScrollPanCam.tsx
+++ src/components/ScrollPanCam.tsx
@@ -5,16 +5,31 @@ import { useFrame, useThree } from '@react-three/fiber'
 function ScrollPanCam() {
 
   const [scrollY, setScrollY] = useState(0);
+  const [touchStartY, setTouchStartY] = useState(0);
   const { camera } = useThree();
   const target = useRef(new THREE.Vector3(0, 0, 0));
 
   const pan_factor = 3;
   const easeFactor = 0.05;
 
-  window.onwheel = (event: WheelEvent) => {
+  const handleWheel = (event: WheelEvent) => {
     const newScrollY = THREE.MathUtils.clamp(scrollY + (event.deltaY * 0.5), 0, 1000)
     setScrollY(newScrollY)
   }
+
+  const handleTouchStart = (event: TouchEvent) => {
+    setTouchStartY(event.touches[0].clientY);
+  }
+
+  const handleTouchMove = (event: TouchEvent) => {
+    const deltaY = event.touches[0].clientY - touchStartY;
+    const newScrollY = THREE.MathUtils.clamp(scrollY + deltaY * 0.1, 0, 1000);
+    setScrollY(newScrollY);
+  };
+
+  window.addEventListener("wheel", handleWheel, { passive: false })
+  window.addEventListener("touchstart", handleTouchStart, { passive: false })
+  window.addEventListener("touchmove", handleTouchMove, { passive: false })
 
   useFrame((state) => {
     const targetPosition = new THREE.Vector3(
